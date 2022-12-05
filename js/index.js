f3e55efd65b1042fe9e6db2ca3e6296a34db3433
eduardo7321 <eduardo.jose184@gmail.com>
2022-12-05 20:28:19 -0300

    atualizando o form e criando mensagem enviada com sucesso

---
+++ js/index.js
@@ -0,0 +1,24 @@
+import './style.css'
+
+export function Contato() {
+  return (
+    <section className="contato" id="Contatos">
+      <div className="container">
+        <h4>contato</h4>
+        <form action="https://formsquash.io/f/MeoMJW8Lxzac9BefOiu1" method="POST">
+          <input type="email" name="email" placeholder="digite o seu email aqui" />
+          <input type="text" name="firstName" placeholder="digite o seu nome aqui" />
+          <textarea placeholder="escreva a sua mensagem aqui" name="mensagem" id="mensagem" type="mensagem" className="text">
+
+
+          </textarea>
+
+          <input type="hidden" name="_gotcha"></input>
+          <input className="enviar" type="submit" value="enviar" />
+        </form>
+
+      </div>
+    </section>
+  )
+
+}
